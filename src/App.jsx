41e0dbd2012559a6a917a8a9e71fa3840710bb18
Santsosh Khadka <santoshkhadka2326@gmail.com>
2024-02-01 10:39:30 +0545

    vanelement_addition_done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-
+import "./server";
+import Vans from "./components/Vans";
 function App() {
   return (
     <BrowserRouter>
@@ -13,11 +14,13 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
